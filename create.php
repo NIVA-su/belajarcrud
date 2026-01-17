354450002acbbd010ad6c26f13c4479e9f8df1aa
mobs-7 <avinlyalvendriez@gmail.com>
2026-01-17 11:42:49 +0700

    create

---
+++ create.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tambah data peserta</title>
+</head>
+<body>
+    <h1>Tambah Data Peserta</h1>
+    <form method="post">
+        <label>nama: </label>
+        <input type="text" name="nama"> <br>
+
+        <label>email: </label>
+        <input type="email" name="email"> <br>
+
+        <label>usia: </label>
+        <input type="number" name="usia"> <br>
+
+        <button type="submit" name="tambah">tambah</button>
+        <a href="index.php">kembali</a>
+    </form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>PNEUMA</h1>
+    <a href="create.php">create</a>
+</body>
+</html>
